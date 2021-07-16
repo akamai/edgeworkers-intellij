50f20fb362d7ee42f3db5a9ee2003512a9f5deb9
ahsankamal <ahsankamal60@gmail.com>
2021-07-16 18:51:15 -0300

    create & validate action submenu, EW cli wrapper

---
+++ src/main/java/actions/CreateBundleAction.java
@@ -0,0 +1,71 @@
+package actions;
+
+import com.intellij.json.JsonFileType;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ui.CreateBundleUI;
+import utils.EdgeworkerWrapper;
+import javax.swing.*;
+
+public class CreateBundleAction extends AnAction {
+
+    public CreateBundleAction() {
+        super();
+    }
+
+    public CreateBundleAction(@Nullable String text, @Nullable String description, @Nullable Icon icon) {
+        super(text, description, icon);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        // make the action item visible if the filetype is JSON
+        PsiFile psiFile =  event.getData(CommonDataKeys.PSI_FILE);
+        event.getPresentation().setEnabledAndVisible(event.getProject()!=null && psiFile.getFileType() instanceof JsonFileType);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = event.getProject();
+        CreateBundleUI createBundleUI = new CreateBundleUI();
+        VirtualFile[] filesToBeCompressed = createBundleUI.filesChooser(event);
+        if (null == filesToBeCompressed || filesToBeCompressed.length==0){
+            Messages.showInfoMessage("No file selected for code bundle", "Info");
+            return;
+        }
+
+        VirtualFile tarballFileLocation = createBundleUI.destinationFolderChooser(event);
+        if (null == tarballFileLocation){
+            Messages.showInfoMessage("No folder selected for code bundle location", "Info");
+            return;
+        }
+
+        PsiFile psiFile =  event.getData(CommonDataKeys.PSI_FILE);
+        VirtualFile parentPath = psiFile.getVirtualFile().getParent();
+        String currentActionDirectory = parentPath.getCanonicalPath();
+
+        EdgeworkerWrapper edgeworkerWrapper = new EdgeworkerWrapper(project);
+
+        try {
+            edgeworkerWrapper.createTarball(currentActionDirectory, filesToBeCompressed, tarballFileLocation);
+        }catch (Exception e){
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            edgeworkerWrapper.validateBundle(currentActionDirectory, tarballFileLocation);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
