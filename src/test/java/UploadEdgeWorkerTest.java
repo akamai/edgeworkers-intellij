35bdf7f81c2098d5a9ec661272a96c6a7ac8b68c
ahsankamal <ahsankamal60@gmail.com>
2021-09-07 17:56:54 -0300

    add upload button on EW management toolbar

--- src/test/java/UploadEdgeWorkerTest.java
+++ src/test/java/UploadEdgeWorkerTest.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class UploadEdgeWorkerTest {
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.testFramework.fixtures.*;
+import config.EdgeWorkersConfig;
+import config.SettingsService;
+import org.junit.After;
+import org.junit.Before;
+import utils.EdgeworkerWrapper;
+
+public class UploadEdgeWorkerTest extends BasePlatformTestCase {
+
+    protected CodeInsightTestFixture myFixture;
+    EdgeWorkersConfig config;
+    EdgeworkerWrapper edgeworkerWrapper;
+
+    @Before
+    public void setUp() throws Exception {
+        IdeaTestFixtureFactory factory = IdeaTestFixtureFactory.getFixtureFactory();
+        TestFixtureBuilder<IdeaProjectTestFixture> fixtureBuilder = factory.createLightFixtureBuilder();
+        IdeaProjectTestFixture fixture = fixtureBuilder.getFixture();
+        myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixture);
+        myFixture.setTestDataPath(getTestDataPath());
+        myFixture.setUp();
+        edgeworkerWrapper = new EdgeworkerWrapper(myFixture.getProject());
+        config = SettingsService.getInstance().getState();
+        config.setAccountKey("");
+        config.setEdgercSectionName("");
+        config.setEdgercFilePath("");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        myFixture.tearDown();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/resources/testData/";
+    }
+
+    public void test_getUploadEdgeWorkerCommand() throws Exception{
+        config.setAccountKey("testKey");
+        GeneralCommandLine commandLine = edgeworkerWrapper.getUploadEdgeWorkerCommand("123" , "tmpFile");
+        assertEquals("akamai [edgeworkers, upload, 123, --bundle, tmpFile, --accountkey, testKey]", commandLine.toString());
+    }
+
 }
