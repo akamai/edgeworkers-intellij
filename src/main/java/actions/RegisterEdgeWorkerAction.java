271fc14fb847903fd96b09e33353c5fb7ac6cfe0
ahsankamal <ahsankamal60@gmail.com>
2021-09-22 17:32:43 -0300

    register edgeworker ui

---
+++ src/main/java/actions/RegisterEdgeWorkerAction.java
@@ -0,0 +1,62 @@
+package actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ui.RegisterEdgeWorkerDialog;
+import utils.EdgeworkerWrapper;
+
+import javax.swing.*;
+import java.util.ResourceBundle;
+
+public class RegisterEdgeWorkerAction extends AnAction {
+
+    private ResourceBundle resourceBundle;
+
+    public RegisterEdgeWorkerAction(@Nullable String text, @Nullable String description, @Nullable Icon icon) {
+        super(text, description, icon);
+        resourceBundle = ResourceBundle.getBundle("ActionBundle");
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        if(new EdgeworkerWrapper().checkIfAkamaiCliInstalled()==false){
+            return;
+        }
+        String groupId = null;
+        String edgeWorkerName = null;
+        Integer resourceTierId = null;
+        try {
+            RegisterEdgeWorkerDialog dialog = new RegisterEdgeWorkerDialog();
+            if(dialog.showAndGet()){
+                //ok button pressed
+                groupId = dialog.getSelectedGroupId();
+                edgeWorkerName = dialog.getEdgeWorkerName();
+                resourceTierId = dialog.getSelectedResourceTierId();
+                System.out.println(groupId+" "+edgeWorkerName+" "+resourceTierId);
+                EdgeworkerWrapper edgeworkerWrapper = new EdgeworkerWrapper(e.getProject());
+                if(null==edgeWorkerName){
+                    Messages.showErrorDialog("The EdgeWorker Name can't be empty.", "Error");
+                    return;
+                }
+                if(null==groupId){
+                    Messages.showErrorDialog("The EdgeWorker Group can't be empty.", "Error");
+                    return;
+                }
+                if(null==resourceTierId){
+                    Messages.showErrorDialog("The Resource Tier can't be empty.", "Error");
+                    return;
+                }
+                edgeworkerWrapper.registerEdgeWorker(groupId, edgeWorkerName, resourceTierId);
+            }else {
+                //cancel button pressed
+                return;
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
